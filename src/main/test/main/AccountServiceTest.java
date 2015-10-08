3909f77f893bf777d6cd9e65b9ff519e5c661153
crisscuola <bestwick91@gmail.com>
2015-10-08 23:03:49 +0300

    add prototypes tests

---
+++ src/main/test/main/AccountServiceTest.java
@@ -0,0 +1,46 @@
+package main;
+
+import org.junit.Test;
+//import main.AccountService;
+
+//import static org.junit.Assert.*;
+
+
+public class AccountServiceTest {
+
+    @Test
+    public void testAddUser() throws Exception {
+
+    }
+
+    @Test
+    public void testAddSessions() throws Exception {
+
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+
+    }
+
+    @Test
+    public void testIsSignedIn() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveSessions() throws Exception {
+
+    }
+
+    @Test
+    public void testCountSignIn() throws Exception {
+
+
+    }
+
+    @Test
+    public void testCountSignUp() throws Exception {
+
+    }
+}
