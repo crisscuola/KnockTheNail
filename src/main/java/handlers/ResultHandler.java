8872b7f6d41d748335875ef6ebffb71ae3fb16d9
crisscuola <bestwick91@gmail.com>
2015-11-18 21:07:40 +0300

    database service begin

---
+++ src/main/java/handlers/ResultHandler.java
@@ -0,0 +1,9 @@
+package handlers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface ResultHandler {
+
+    void handle(ResultSet result) throws SQLException;
+}
---
+++ src/main/java/handlers/TResultHandler.java
@@ -0,0 +1,9 @@
+package handlers;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface TResultHandler<T> {
+    T handle(ResultSet resultSet) throws SQLException;
+}
